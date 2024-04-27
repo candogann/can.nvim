79c815c6e229761242e74db4cd9e8473fa00627d
Ali Can Dogan <can.dogan@servissoft.net>
2024-04-27 23:34:17 +0300

    neotree implemented

--- init.lua
+++ init.lua
@@ -93,6 +93,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
+vim.keymap.set('n', '<leader>y', '+y')
+vim.api.nvim_set_option("clipboard", "unnamed")
+
 -- [[ Setting options ]]
 require 'options'
 
---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,35 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+--
+return { {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+
+	cmd = 'Neotree',
+
+	keys = {
+		{ '<leader>nl', '<cmd>Neotree filesystem reveal right<CR>', desc = 'Reveal filesystem' }
+	},
+	opts = {
+		event_handlers = {
+			{
+				event = "file_opened",
+				handler = function(file_path)
+					--auto close
+					require("neo-tree").close_all()
+				end
+			}
+		},
+		filesystem = {
+		}
+	}
+}
+}
